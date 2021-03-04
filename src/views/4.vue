202a9e753c457e7b4ad1ee5672452af61c0f7c6e
Arnau Gómez Farell <agomez1f@gmail.com>
2021-03-04 08:03:37 +0100

    repte 4

--- src/views/4.vue
+++ src/views/4.vue
@@ -4,17 +4,25 @@
     message="
       Sembla que tenim part de la resposta que buscàvem. Quina emoció!
     "
-    correct-answer="1"
-    next-page="3"
+    correct-answer="16777216"
+    next-page="5"
   >
-    <figure class="w-80 my-4">
-      <img
-        src="@/assets/piramide.png"
-        alt="formatge"
-        style="mix-blend-mode: multiply;"
-      />
-    </figure>
-    <p></p>
+    <p>
+      Els colors d'una pantalla o fotografia es representen amb un número.
+      D'aquesta manera, quan enfosquim, aclarem o apliquem un filtre a una foto,
+      l'ordinador està fent operacions matemàtiques que transformen aquests
+      colors.<br />
+      Ara hem de cercar quines són aquestes operacions matemàtiques que
+      transformen aquests colors per poder tornar les fotografies al seu estat
+      original
+    </p>
+    <p>
+      <strong>Quants colors diferents pot representar un ordinador?</strong>
+    </p>
+    <input
+      type="color"
+      class="block w-16 h-16 border-2 border-dashed border-denim m-8 mx-auto"
+    />
   </Dashboard>
 </template>
 
